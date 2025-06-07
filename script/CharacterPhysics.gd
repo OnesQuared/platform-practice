7f4d764fb533b432a2764532e13a13e1614dfb64
Paul Moon <bakamono.moon@gmail.com>
2025-06-06 22:53:42 -0400

    fix change of level
    removed the input method that is not working to change level. Using on entry method for change of level

--- script/CharacterPhysics.gd
+++ script/CharacterPhysics.gd
@@ -22,3 +22,11 @@ func killPlayer():
 
 func _on_death_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	killPlayer()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://scene/Stage2.tscn")
+
+
+func _on_death_body_entered(body: Node2D) -> void:
+	killPlayer()
